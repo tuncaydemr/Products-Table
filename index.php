374000140aab2d935d8665d388b7229db27e324c
Fogo Software <tuncaydemir682@gmail.com>
2023-06-08 02:29:04 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+  include "./classes/db.class.php";
+  include "./classes/product.class.php";
+?>
+
+<?php include "./includes/header.php"; ?>
+
+<div class="container mt-5">
+
+  <a href="create-product.php" class="btn btn-primary">Create Product</a>
+
+  <hr>
+
+  <?php $product = new Product(); ?>
+
+  <?php if ($product->getProducts()) : ?>
+    <?php include "./includes/product-list.php"; ?>
+  <?php else : ?>
+    <?php include "./includes/no-product.php"; ?>
+  <?php endif; ?>
+
+</div>
+
+<?php include "./includes/footer.php"; ?>
