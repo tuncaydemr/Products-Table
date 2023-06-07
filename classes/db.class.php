a364906747bf37ec4e160e7d4171115a25ee4b2d
Fogo Software <tuncaydemir682@gmail.com>
2023-06-08 02:28:47 +0300

    Create db.class.php

---
+++ classes/db.class.php
@@ -0,0 +1,16 @@
+<?php
+  class Database {
+    protected function connect() {
+      try {
+        $db = new PDO("mysql:host=localhost;dbname=mydb;", "root", "");
+        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
+
+        return $db;
+      } catch (PDOException $err) {
+        echo $err->getMessage();
+      }
+    }
+  }
+
+?>
